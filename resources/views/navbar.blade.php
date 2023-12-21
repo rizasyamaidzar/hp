e08aa2fb6b9cc8f92e2664ff3cf3f821547a3533
rizasyamaidzar <rizasyamaidzar@gmail.com>
2023-12-21 12:19:05 +0700

    navbar edit

--- resources/views/navbar.blade.php
+++ resources/views/navbar.blade.php
@@ -32,8 +32,11 @@
               </div>
               <ul class="py-1" role="none">
                 <li>
-                  <a href="#" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:text-gray-300 dark:hover:bg-gray-600 dark:hover:text-white" role="menuitem">Sign out</a>
-                </li>
+                <form action="/logout" method="post">
+                  @csrf
+                  <button type="submit" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:text-gray-300 dark:hover:bg-gray-600 dark:hover:text-white" role="menuitem">Sign out</button>
+                </form> 
+               </li>
               </ul>
             </div>
           </div>
